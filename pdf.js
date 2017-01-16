7ce3e065bdb2f53154d1db89c48abfd5aa02699d
Algram <aliasgram@gmail.com>
2017-01-16 14:44:29 +0100

    Added correct sizes

--- pdf.js
+++ pdf.js
@@ -5,10 +5,13 @@ const PDFDocument = require('pdfkit');
 function generate(data, cb) {
   const doc = new PDFDocument({
     layout: 'landscape',
-    size: [156, 241]
+    size: [172.91304, 257.95296],
+    margin: 15
   });
 
-  const page = doc.page;
+  let page = doc.page;
+  page.dictionary.data.TrimBox = [8.50392, 8.50392, 240.94512+8.50392, 155.90520+8.50392];
+  page.dictionary.data.BleedBox = [0, 0, 257.95296, 172.91304];
 
   doc.rect(0, 0, page.width, page.height)
     .fill('#111111');
@@ -18,9 +21,12 @@ function generate(data, cb) {
     .text(new Date().toLocaleString());
 
   doc.addPage();
+  page = doc.page;
+  page.dictionary.data.TrimBox = [8.50392, 8.50392, 240.94512+8.50392, 155.90520+8.50392];
+  page.dictionary.data.BleedBox = [0, 0, 257.95296, 172.91304];
 
   request(`https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl=${data.test}`, (err, response, buffer) => {
-    doc.image(buffer, 0, 0, { width: 150 });
+    doc.image(buffer, 0, 0, { width: 80 });
     doc.end();
     cb();
   });
