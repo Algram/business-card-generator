aadcd254a0f803ef21058419861502f76e8162d9
Algram <aliasgram@gmail.com>
2017-01-18 14:52:10 +0100

    Added backside cut marks

--- pdf2.js
+++ pdf2.js
@@ -177,6 +177,123 @@ function generate(data, cb) {
 
   request(`https://chart.googleapis.com/chart?chs=400x400&cht=qr&chl=${data.name}`, (err, response, buffer) => {
     doc.image(buffer, (doc.page.width - 110), (doc.page.height - 80) / 2, { width: 80 });
+
+
+    /* Ab hier weiße Schnittmarken */
+    /* Schnittmarke lovw */
+    doc.lineWidth(1.5)
+      .moveTo(8.50392, 0)
+      .lineTo(8.50392, 5.6692944)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke luvw */
+    doc.lineWidth(1.5)
+      .moveTo(8.50392, 167.24376)
+      .lineTo(8.50392, 172.91304)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke ruvw */
+    doc.lineWidth(1.5)
+      .moveTo(249.44904, 167.24376)
+      .lineTo(249.44904, 172.91304)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke rovw */
+    doc.lineWidth(1.5)
+      .moveTo(249.44904, 0)
+      .lineTo(249.44904, 5.6692944)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke lohw */
+    doc.lineWidth(1.5)
+      .moveTo(0, 8.50392)
+      .lineTo(5.6692944, 8.50392)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke luhw */
+    doc.lineWidth(1.5)
+      .moveTo(0, 164.40912)
+      .lineTo(5.6692944, 164.40912)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke ruhw */
+    doc.lineWidth(1.5)
+      .moveTo(252.28368, 164.40912)
+      .lineTo(257.95296, 164.40912)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+    /* Schnittmarke rohw */
+    doc.lineWidth(1.5)
+      .moveTo(252.28368, 8.50392)
+      .lineTo(257.95296, 8.50392)
+      .fillAndStroke([0, 0, 0, 0])
+      .stroke();
+
+
+    /* Ab hier schwarze Schnittmarken */
+    /* Schnittmarke lovs */
+    doc.lineWidth(0.5)
+      .moveTo(8.50392, 0)
+      .lineTo(8.50392, 5.6692944)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke luvs */
+    doc.lineWidth(0.5)
+      .moveTo(8.50392, 167.24376)
+      .lineTo(8.50392, 172.91304)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke ruvs */
+    doc.lineWidth(0.5)
+      .moveTo(249.44904, 167.24376)
+      .lineTo(249.44904, 172.91304)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke rovs */
+    doc.lineWidth(0.5)
+      .moveTo(249.44904, 0)
+      .lineTo(249.44904, 5.6692944)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke lohs */
+    doc.lineWidth(0.5)
+      .moveTo(0, 8.50392)
+      .lineTo(5.6692944, 8.50392)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke luhs */
+    doc.lineWidth(0.5)
+      .moveTo(0, 164.40912)
+      .lineTo(5.6692944, 164.40912)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke ruhs */
+    doc.lineWidth(0.5)
+      .moveTo(252.28368, 164.40912)
+      .lineTo(257.95296, 164.40912)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
+    /* Schnittmarke rohs */
+    doc.lineWidth(0.5)
+      .moveTo(252.28368, 8.50392)
+      .lineTo(257.95296, 8.50392)
+      .fillAndStroke([100, 100, 100, 100])
+      .stroke();
+
     doc.end();
     setTimeout(() => {
       cb();
